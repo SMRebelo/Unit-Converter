0a7201c8859e398509c9ee450aede9385d85d979
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-19 00:47:11 +0100

    Fixing the layout + starting on the js

---
+++ js/pixels.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("logoOne");
+
+button.addEventListener("click", (event) => {
+  document.getElementById("titleRight").innerHTML = `Pixel Converter`;
+  document.getElementById("opcionInputOne").innerHTML = `Px`;
+  document.getElementById("opcionInputTwo").innerHTML = `Em`;
+  document.getElementById("opcionInputThree").innerHTML = `Rm`;
+  document.getElementById("opcionOutputOne").innerHTML = `Px`;
+  document.getElementById("opcionOutputTwo").innerHTML = `Em`;
+  document.getElementById("opcionOutputThree").innerHTML = `Rm`;
+
+    let valueOne = +(document.getElementById("inputSelection").value);
+
+});
--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import './styles/style.css'
 import './styles/columnLeft.css'
 import './styles/columnRight.css'
 import './styles/columnCenter.css'
+import './js/pixels.js'
+
 
