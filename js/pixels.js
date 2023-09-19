e23c15cfea9a0fed60132faa2e376deb447347e6
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-20 00:37:03 +0100

    trying to find the bug in the formula

--- js/pixels.js
+++ js/pixels.js
@@ -9,35 +9,77 @@ button.addEventListener("click", (event) => {
   document.getElementById("opcionOutputOne").innerHTML = `Px`;
   document.getElementById("opcionOutputTwo").innerHTML = `Em`;
   document.getElementById("opcionOutputThree").innerHTML = `Rem`;
-
+  console.log("inicio");
+  alert("anicio");
   function pixels() {
-    let valueOne = (+document.getElementById("inputSelection")).value;
+    let valueOne = +(document.getElementById("inputSelection")).value;
     let inputOne = document.getElementById("inputOne");
     let inputTwo = document.getElementById("inputTwo");
-    
+    let res=0;
+
     if(inputOne == "Px"){
-      let res=0;
+      
       switch (inputTwo) {
         case "Em":
           res=valueOne/16;
           document.getElementById("result").innerHTML = res;
+          alert("em")
           break;
         case "Rem":
-
+          res = valueOne/16;
+          document.getElementById("result").innerHTML = res;
+          alert("rem")
+          break;
+          case "Px":
+          alert("Error chose another type.")
+          document.getElementById("inputTwo").focus();
+          alert("null")
           break;
         default:
-          break;
+          alert("default")
       }
-    }
-  
+    }else if(inputOne == "Em"){
+
+      switch (inputTwo) {
+        case "Px":
+            res = valueOne*16;
+            document.getElementById("result").innerHTML = res;
+          break;
+          case "Em":
+            alert("Error chose another type.")
+            document.getElementById("inputTwo").focus();
+          break;
+          case "Rem":
+            res = valueOne*16;
+            document.getElementById("result").innerHTML = res;
+          break;
+      
+        default:
+          break;
+          alert("default")
+      }
+    }else if(inputOne == "Rem"){
+
+      switch (inputTwo) {
+        case "Px":
+            res = valueOne*16;
+            document.getElementById("result").innerHTML = res;
+          break;            
+          case "Rem":
+            res = valueOne*16;
+            document.getElementById("result").innerHTML = res;
+          break;
+          case "Em":
+            alert("Error chose another type.")
+            document.getElementById("inputTwo").focus();
+          break;
+      
+        default:
+          break;
+          alert("default")
+      }
   }
-
-
-
-
-
-
   buttonSubmit.addEventListener("click", () => {
     pixels();
   })
-});
+}});
