8306808301245b9d9bada6af23ec21061d542b51
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-20 00:05:31 +0100

    fail to fetch code writen in phone + rewrite all the code

--- js/pixels.js
+++ js/pixels.js
@@ -1,15 +1,43 @@
 const button = document.getElementById("logoOne");
+const buttonSubmit = document.getElementById("buttonSubmit");
 
 button.addEventListener("click", (event) => {
   document.getElementById("titleRight").innerHTML = `Pixel Converter`;
   document.getElementById("opcionInputOne").innerHTML = `Px`;
   document.getElementById("opcionInputTwo").innerHTML = `Em`;
-  document.getElementById("opcionInputThree").innerHTML = `Rm`;
+  document.getElementById("opcionInputThree").innerHTML = `Rem`;
   document.getElementById("opcionOutputOne").innerHTML = `Px`;
   document.getElementById("opcionOutputTwo").innerHTML = `Em`;
-  document.getElementById("opcionOutputThree").innerHTML = `Rm`;
+  document.getElementById("opcionOutputThree").innerHTML = `Rem`;
 
-    let valueOne = +(document.getElementById("inputSelection").value);
+  function pixels() {
+    let valueOne = (+document.getElementById("inputSelection")).value;
+    let inputOne = document.getElementById("inputOne");
+    let inputTwo = document.getElementById("inputTwo");
+    
+    if(inputOne == "Px"){
+      let res=0;
+      switch (inputTwo) {
+        case "Em":
+          res=valueOne/16;
+          document.getElementById("result").innerHTML = res;
+          break;
+        case "Rem":
 
+          break;
+        default:
+          break;
+      }
+    }
+  
+  }
+
+
+
+
+
+
+  buttonSubmit.addEventListener("click", () => {
+    pixels();
+  })
 });
-
