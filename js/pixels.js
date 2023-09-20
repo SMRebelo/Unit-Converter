a2507da7e04ef61b95b8e2d0b6059dabaf312409
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-20 20:07:51 +0100

    add better formula + pixels converter done for now

--- js/pixels.js
+++ js/pixels.js
@@ -1,6 +1,5 @@
 const button = document.getElementById("logoOne");
 const buttonSubmit = document.getElementById("buttonSubmit");
-let resFinal = 0;
 
 button.addEventListener("click", (event) => {
   document.getElementById("titleRight").innerHTML = `Pixel Converter`;
@@ -14,69 +13,54 @@ button.addEventListener("click", (event) => {
 });
 
 buttonSubmit.addEventListener("click", (event) => {
-  event.preventDefault();// The big mistake was that i was making a "form", and in a form we send the information to a API. of course it does stick on the screen!!! to prevent that we call the method "EVENT.PREVENTDEFAULT();" this makes the information displayed stick on the screen
+  event.preventDefault(); //? The big mistake was that i was making a "form", and in a form we send the information to a API. of course it does stick on the screen!!! to prevent that we call the method "EVENT.PREVENTDEFAULT();" this makes the information displayed stick on the screen
 
   function pixels() {
     let valueOne = +document.getElementById("inputSelection").value;
     let inputOneRes = document.getElementById("inputOne").value;
     let inputTwoRes = document.getElementById("inputTwo").value;
     let res = 0;
+    let formula = "";
 
     if (inputOneRes === "Px") {
       switch (inputTwoRes) {
         case "Em":
-          res = valueOne / 16;
-          break;
-
         case "Rem":
-          res = valueOne / 16;
+          formula = `Px = ${inputTwoRes} / Font-size `;
+          res = valueOne / 16; //! DONT FORGET TO ADD SOMETHING TO GET THE FONT-SIZE FROM THE BROWSER
           break;
-        case "Px":
+        default:
           alert("Error chose another type.");
           document.getElementById("inputTwo").focus();
-          break;
-
-        default:
-          alert("default case one");
       }
-      resFinal = res;
     }
     if (inputOneRes === "Em") {
       switch (inputTwoRes) {
         case "Px":
-          res = valueOne * 16;
-          break;
-        case "Em":
-          alert("Error chose another type.");
-          document.getElementById("inputTwo").focus();
-          break;
         case "Rem":
+          formula = `Em = ${inputTwoRes} * Font-size `;
           res = valueOne * 16;
           break;
         default:
-          alert("default case two");
+          alert("Error chose another type.");
+          document.getElementById("inputTwo").focus();
       }
-      resFinal = res;
     }
     if (inputOneRes === "Rem") {
       switch (inputTwoRes) {
         case "Px":
-          res = valueOne * 16;
-          break;
         case "Em":
+          formula = `Rem = ${inputTwoRes} * Font-size`;
           res = valueOne * 16;
-          break;
-        case "Rem":
-          alert("Error chose another type.");
-          document.getElementById("inputTwo").focus();
           break;
 
         default:
-          alert("default case three");
+          alert("Error chose another type.");
+          document.getElementById("inputTwo").focus();
       }
-      resFinal = res;
     }
-    document.getElementById("result").innerHTML = resFinal;
+    document.getElementById("result").innerHTML = res;
+    document.getElementById("calculationWindow").innerHTML = formula;
   } // fim da função pixels
-  pixels();
+  pixels(); //? ONE OF THE BIG PROBLEMS WAS THAT I WASN´T CALLING THE FUNCTION IN THE END OF THE CLICK BUT OUTSIDE THE FUNCTION!!!
 });
