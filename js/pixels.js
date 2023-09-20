e1a53b5397f5e1ae5eb296d6e6aa1ddc640319a5
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-20 19:47:54 +0100

    form mistake solved

--- js/pixels.js
+++ js/pixels.js
@@ -13,71 +13,70 @@ button.addEventListener("click", (event) => {
   console.log("inicio");
 });
 
-buttonSubmit.addEventListener("click", () => {
+buttonSubmit.addEventListener("click", (event) => {
+  event.preventDefault();// The big mistake was that i was making a "form", and in a form we send the information to a API. of course it does stick on the screen!!! to prevent that we call the method "EVENT.PREVENTDEFAULT();" this makes the information displayed stick on the screen
+
+  function pixels() {
+    let valueOne = +document.getElementById("inputSelection").value;
+    let inputOneRes = document.getElementById("inputOne").value;
+    let inputTwoRes = document.getElementById("inputTwo").value;
+    let res = 0;
+
+    if (inputOneRes === "Px") {
+      switch (inputTwoRes) {
+        case "Em":
+          res = valueOne / 16;
+          break;
+
+        case "Rem":
+          res = valueOne / 16;
+          break;
+        case "Px":
+          alert("Error chose another type.");
+          document.getElementById("inputTwo").focus();
+          break;
+
+        default:
+          alert("default case one");
+      }
+      resFinal = res;
+    }
+    if (inputOneRes === "Em") {
+      switch (inputTwoRes) {
+        case "Px":
+          res = valueOne * 16;
+          break;
+        case "Em":
+          alert("Error chose another type.");
+          document.getElementById("inputTwo").focus();
+          break;
+        case "Rem":
+          res = valueOne * 16;
+          break;
+        default:
+          alert("default case two");
+      }
+      resFinal = res;
+    }
+    if (inputOneRes === "Rem") {
+      switch (inputTwoRes) {
+        case "Px":
+          res = valueOne * 16;
+          break;
+        case "Em":
+          res = valueOne * 16;
+          break;
+        case "Rem":
+          alert("Error chose another type.");
+          document.getElementById("inputTwo").focus();
+          break;
+
+        default:
+          alert("default case three");
+      }
+      resFinal = res;
+    }
+    document.getElementById("result").innerHTML = resFinal;
+  } // fim da função pixels
   pixels();
 });
-
-function pixels() {
-  let valueOne = +(document.getElementById("inputSelection").value);
-  let inputOneRes = document.getElementById("inputOne").value;
-  let inputTwoRes = document.getElementById("inputTwo").value;
-  let res = 0;
-
-  if (inputOneRes === "Px") {
-
-    switch (inputTwoRes) {
-      case "Em": 
-        res = valueOne / 16;
-        break;
-      
-      case "Rem":
-        res = valueOne / 16;
-        break;
-      case "Px": 
-        alert("Error chose another type.");
-        document.getElementById("inputTwo").focus();
-        break;
-      
-      default: 
-        alert("default case one");
-      
-    }
-    res = resFinal;
-  }
-  if (inputOneRes === "Em") {
-    switch (inputTwoRes) {
-      case "Px":
-        res = valueOne * 16;
-        break;
-      case "Em":
-        alert("Error chose another type.");
-        document.getElementById("inputTwo").focus();
-        break;
-      case "Rem":
-        res = valueOne * 16;
-        break;
-      default:
-        alert("default case two");
-    }
-    res = resFinal;
-  }
-  if (inputOneRes === "Rem") {
-    switch (inputTwoRes) {
-      case "Px":
-        res = valueOne * 16;
-        break;
-      case "Em":
-        res = valueOne * 16;
-        break;
-      case "Rem":
-        alert("Error chose another type.");
-        document.getElementById("inputTwo").focus();
-        break;
-
-      default:
-        alert("default case three");
-    }
-    res = resFinal;
-  }
-  document.getElementById("result").innerHTML = resFinal;
-} // fim da função pixels
