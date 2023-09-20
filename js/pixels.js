6f171e64f9b458e28ec438984767088144c4e002
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-20 15:18:22 +0100

    trying other methods to fix the formula

--- js/pixels.js
+++ js/pixels.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("logoOne");
 const buttonSubmit = document.getElementById("buttonSubmit");
-const resFinal = 0;
+let resFinal = 0;
 
 button.addEventListener("click", (event) => {
   document.getElementById("titleRight").innerHTML = `Pixel Converter`;
@@ -11,75 +11,73 @@ button.addEventListener("click", (event) => {
   document.getElementById("opcionOutputTwo").innerHTML = `Em`;
   document.getElementById("opcionOutputThree").innerHTML = `Rem`;
   console.log("inicio");
-
-
-  function pixels() {
-    const valueOne = +document.getElementById("inputSelection").value;
-    const inputOneRes = document.getElementById("inputOne");
-    const inputTwoRes = document.getElementById("inputTwo");
-    var res = 0;
-
-    if (inputOneRes === "Px") {
-      switch (inputTwoRes) {
-        case "Em":{
-          res = valueOne / 16;
-          break;
-          }
-        case "Rem":{
-          res = valueOne / 16;
-          break;
-          }
-        case "Px":{
-          alert("Error chose another type.");
-          document.getElementById("inputTwo").focus();
-          break;
-          }
-        default:{
-          alert("default case one");}
-      }
-      res = resFinal;
-
-    } else if (inputOneRes === "Em") {
-      switch (inputTwoRes) {
-        case "Px":
-          res = valueOne * 16;
-          break;
-        case "Em":
-          alert("Error chose another type.");
-          document.getElementById("inputTwo").focus();
-          break;
-        case "Rem":
-          res = valueOne * 16;
-          break;
-        default:
-          alert("default case two");
-      }
-      res = resFinal;
-    } 
-    else if (inputOneRes === "Rem") {
-      switch (inputTwoRes) {
-        case "Px":
-          res = valueOne * 16;
-          break;
-        case "Em":
-          res = valueOne * 16;
-          break;
-        case "Rem":
-          alert("Error chose another type.");
-          document.getElementById("inputTwo").focus();
-          break;
-
-        default:
-          alert("default case three");
-      }
-      res = resFinal;
-    }
-  } // fim da função pixels
-  document.getElementById("result").innerHTML = resFinal;
-  buttonSubmit.addEventListener("click", () => {
-    pixels();
-    document.getElementById("result").innerHTML = resFinal;
-  });
-  document.getElementById("result").innerHTML = resFinal;
 });
-document.getElementById("result").innerHTML = resFinal;
+
+buttonSubmit.addEventListener("click", () => {
+  pixels();
+});
+
+function pixels() {
+  let valueOne = +(document.getElementById("inputSelection").value);
+  let inputOneRes = document.getElementById("inputOne").value;
+  let inputTwoRes = document.getElementById("inputTwo").value;
+  let res = 0;
+
+  if (inputOneRes === "Px") {
+
+    switch (inputTwoRes) {
+      case "Em": 
+        res = valueOne / 16;
+        break;
+      
+      case "Rem":
+        res = valueOne / 16;
+        break;
+      case "Px": 
+        alert("Error chose another type.");
+        document.getElementById("inputTwo").focus();
+        break;
+      
+      default: 
+        alert("default case one");
+      
+    }
+    res = resFinal;
+  }
+  if (inputOneRes === "Em") {
+    switch (inputTwoRes) {
+      case "Px":
+        res = valueOne * 16;
+        break;
+      case "Em":
+        alert("Error chose another type.");
+        document.getElementById("inputTwo").focus();
+        break;
+      case "Rem":
+        res = valueOne * 16;
+        break;
+      default:
+        alert("default case two");
+    }
+    res = resFinal;
+  }
+  if (inputOneRes === "Rem") {
+    switch (inputTwoRes) {
+      case "Px":
+        res = valueOne * 16;
+        break;
+      case "Em":
+        res = valueOne * 16;
+        break;
+      case "Rem":
+        alert("Error chose another type.");
+        document.getElementById("inputTwo").focus();
+        break;
+
+      default:
+        alert("default case three");
+    }
+    res = resFinal;
+  }
+  document.getElementById("result").innerHTML = resFinal;
+} // fim da função pixels
